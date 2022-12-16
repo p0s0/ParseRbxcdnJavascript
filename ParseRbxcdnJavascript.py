303a39ca0bd3aeb5bbf573a26cc2faca5df62c85
p0s0 <73150246+p0s0@users.noreply.github.com>
2022-12-16 13:52:17 -0500

    Add CSS bundle compatibility

--- ParseRbxcdnJavascript.py
+++ ParseRbxcdnJavascript.py
@@ -1,12 +1,14 @@
 # ParseRbxcdnJavascript.py
 # pos0#0998
 # 12/13/2022
-# Parses JavaScript packs from ROBLOX cdn servers
+# Parses JavaScript and CSS packs from ROBLOX cdn servers
 
 import os
 import requests
 
-examplePack = """;// bundle: page___test_m
+# JavaScript
+
+exampleJavaScriptPack = """;// bundle: page___test_m
 ;// files: Test/Test.js, ~/Test/Test.js
 
 ;// Test/Test.js
@@ -15,7 +17,7 @@ console.log("From Test/Test.js!");
 ;// ~/Test/Test.js
 console.log("From ~/Test/Test.js");""" # TODO: make this have an actual use
 
-def parseBundleName(pack: str = examplePack):
+def parseJavaScriptBundleName(pack: str = exampleJavaScriptPack):
     bundleName = ""
 
     for packLine in pack.splitlines():
@@ -24,7 +26,7 @@ def parseBundleName(pack: str = examplePack):
 
     return bundleName
 
-def parseFilesInPack(pack: str = examplePack):
+def parseFilesInJavaScriptPack(pack: str = exampleJavaScriptPack):
     filesInPack = []
 
     for packLine in pack.splitlines():
@@ -33,7 +35,7 @@ def parseFilesInPack(pack: str = examplePack):
 
     return filesInPack
 
-def findJavaScriptFileInPack(pack: str = examplePack, file: str = "Test/Test.js"):
+def findJavaScriptFileInPack(pack: str = exampleJavaScriptPack, file: str = "Test/Test.js"):
     javaScriptFile = ""
     lines = pack.splitlines()
     iterator = iter(lines)
@@ -45,44 +47,102 @@ def findJavaScriptFileInPack(pack: str = examplePack, file: str = "Test/Test.js"
 
     return javaScriptFile
 
+# CSS
+
+exampleCSSBundleURL = "http://www.roblox.com/CSS/Base/CSS/FetchCSS?path=main___23e1cfc4f7f34bdc2ff4f3c2d637a122_m.css"
+exampleCSSBundle = """/* ~/CSS/Test.css */
+.testClass\{float\:left\}""" # TODO: make this have an actual use
+
+def parseCSSBundleName(url: str = exampleCSSBundleURL):
+    return url.split("?path=")[1]
+
+def parseFilesInCSSBundle(bundle: str = exampleCSSBundle):
+    filesInBundle = []
+
+    for bundleLine in bundle.splitlines():
+        if bundleLine.startswith("/* ") and bundleLine.endswith(".css */"):
+            # i hate this So much
+            filesInBundle.append(bundleLine[3:].split(" */")[0])
+
+    return filesInBundle
+
+def findCSSFileInBundle(bundle: str = exampleCSSBundle, file: str = "~/CSS/Test.css"):
+    lines = bundle.splitlines()
+    iterator = iter(lines)
+
+    for bundleLine in lines:
+        next(iterator, None)
+        if bundleLine == "/* " + file + " */":
+            return next(iterator, None).strip()
+
+# Runner
+
 if __name__ == "__main__":
-    javaScriptPack = requests.get(input("Paste the URL to the JavaScript pack here (ex: http://js.rbxcdn.com/pack.js): ")).text
     outputPath = input("Output path (ex: C:/parser/located/here/outputPath): ")
+    bundleType = int(input("CSS (0) or JavaScript (1)?: "))
 
-    if os.path.exists(outputPath):
-        bundleName = parseBundleName(javaScriptPack)
+    if bundleType == 0: # CSS
+        cssBundleURL = input("Paste the URL to the CSS bundle here (ex: http://www.roblox.com/CSS/Base/CSS/FetchCSS?path=page___bundle_m.css): ")
+        cssBundle = requests.get(cssBundleURL).text
 
-        if bundleName != "":
-            filesInPack = parseFilesInPack(javaScriptPack)
-            
-            if len(filesInPack) != 0:
-                filesAsString = ", ".join(filesInPack)
+        if os.path.exists(outputPath):
+            bundleName = parseCSSBundleName(cssBundleURL)
 
-                print("Parsing bundle " + bundleName + " with files " + filesAsString + "...")
+            if bundleName != "":
+                filesInBundle = parseFilesInCSSBundle(cssBundle)
 
-                for file in filesInPack:
-                    javaScriptFile = findJavaScriptFileInPack(javaScriptPack, file)
+                if len(filesInBundle) != 0:
+                    filesAsString = ", ".join(filesInBundle)
 
-                    if javaScriptFile != "":
-                        if file[:1] != "~":
-                            os.makedirs(os.path.dirname(outputPath + "/js/" + file), exist_ok=True)
+                    print("Parsing bundle " + bundleName + " with files " + filesAsString + "...")
 
-                            with open(outputPath + "/js/" + file, 'x') as outputFile:
-                                outputFile.write(javaScriptFile)
-                        else:
+                    for file in filesInBundle:
+                        cssFile = findCSSFileInBundle(cssBundle, file)
+
+                        if cssFile != "":
                             os.makedirs(os.path.dirname(outputPath + file[1:]), exist_ok=True)
 
                             with open(outputPath + file[1:], 'x') as outputFile:
-                                outputFile.write(javaScriptFile)
-                    else:
-                        print("Couldn't find file " + file)
+                                outputFile.write(cssFile)
+        else:
+            print("Invalid output path")
+    elif bundleType == 1: # JavaScript
+        javaScriptPack = requests.get(input("Paste the URL to the JavaScript pack here (ex: http://js.rbxcdn.com/pack.js): ")).text
 
-                print("Done!")
+        if os.path.exists(outputPath):
+            bundleName = parseJavaScriptBundleName(javaScriptPack)
+
+            if bundleName != "":
+                filesInPack = parseFilesInJavaScriptPack(javaScriptPack)
+                
+                if len(filesInPack) != 0:
+                    filesAsString = ", ".join(filesInPack)
+
+                    print("Parsing pack " + bundleName + " with files " + filesAsString + "...")
+
+                    for file in filesInPack:
+                        javaScriptFile = findJavaScriptFileInPack(javaScriptPack, file)
+
+                        if javaScriptFile != "":
+                            if file[:1] != "~":
+                                os.makedirs(os.path.dirname(outputPath + "/js/" + file), exist_ok=True)
+
+                                with open(outputPath + "/js/" + file, 'x') as outputFile:
+                                    outputFile.write(javaScriptFile)
+                            else:
+                                os.makedirs(os.path.dirname(outputPath + file[1:]), exist_ok=True)
+
+                                with open(outputPath + file[1:], 'x') as outputFile:
+                                    outputFile.write(javaScriptFile)
+                        else:
+                            print("Couldn't find file " + file)
+
+                    print("Done!")
+                else:
+                    print("Invalid bundle")
             else:
                 print("Invalid bundle")
         else:
-            print("Invalid bundle")
-    else:
-        print("Invalid output path")
+            print("Invalid output path")
 else:
     exit()
